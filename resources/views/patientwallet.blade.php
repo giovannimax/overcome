ee2e6b9a51dfc0f2fe74116ecaf13d526f296137
jlizl <jlizlmarquez@gmail.com>
2018-02-19 00:54:42 +0800

    datatables for psychwallet and patientwallet, modified datatable css and datatable button css

--- resources/views/patientwallet.blade.php
+++ resources/views/patientwallet.blade.php
@@ -7,8 +7,14 @@
     <div class="container">
       
         <div class="sumrow row"></div>
-         
-        
+        <div class="row" style="margin-bottom: 70px;">
+        <div class="col-md-3"><h4>Payment Summary</h4></div>
+        <div class="col-md-6"><select class="form-control">
+   <option>Sort By: This Month</option>
+   <option>Sort By: This Year</option>
+   <option>Sort By: Last Year</option>
+    </select></div>
+        </div>
      <table class="" id="printsumTable">
         <thead class="summarythead text-center">
           <tr>
@@ -37,23 +43,8 @@
         
     </tbody>
     </table>
-<div id="editor"></div>
 
-    <!-- <nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-end">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav> -->
- 
-    </div> <!-- End of container -->
+  
 @endsection
 
 @section ('scripts')
--- resources/views/psychwallet.blade.php
+++ resources/views/psychwallet.blade.php
@@ -1,87 +1,72 @@
-@extends ('layouts.navbar')
+@extends ('layouts.pnavbar')
+@push('css')
+  <link rel="stylesheet" href="{{ asset('css/datatable.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/datatable-button.css') }}">
+@endpush
 @section ('content')
-
-
     <div class="container">
-        <span class="charchar"><button class="btnPrint btn btn-outline-info btn-md"><i class="material-icons">print</i></button></span>
-        <span class="charchar"><button class="btnDownload btn btn-outline-info btn-md"><i class="material-icons">file_download</i></button></span>
-        <div class="sumrow row">
-          <h4 id="summary">Payment Summary</h4>
-          {!! Form::open(['url' => 'paymentsummary/submit']) !!}
-                  
-            {{Form::select('sortby',[ 'T' => 'Today', 'W' => 'This Week' , 'M' => 'This Month', 'Y' => 'This Year', 'LY' =>'Last Year', 'A' =>'Show All'], 'T')}}
-            
-         {!! Form::close() !!}
-        </div> <!-- End of row -->
-        
-        
-     <table class="sumtable table table-hover" id="printsumTable">
+      
+        <div class="sumrow row"></div>
+        <div class="row" style="margin-bottom: 70px;">
+        <div class="col-md-3"><h4>Payment Summary</h4></div>
+        <div class="col-md-6"><select class="form-control">
+   <option>Sort By: This Month</option>
+   <option>Sort By: This Year</option>
+   <option>Sort By: Last Year</option>
+    </select></div>
+        </div>
+     <table class="" id="printsumTable">
         <thead class="summarythead text-center">
-             <th>Transaction No.</th>
+          <tr>
              <th>Date</th>
-             <th>Patient's Name</th>
+             <th>Psychologist</th>
+             <th>Service</th>
              <th>Total Amount</th>
-        </thead>
+             </tr>
+            </thead>
         <tbody class="text-center summarytbody">
         <tr>
-             <td>12345</td>
              <td>12/28/17</td>
-             <td>John Doe</td>
+             <td>John Snow</td>
+             <td>E-Counseling</td>
              <td>P 1500</td>
         
         </tr>
             
         <tr>
-             <td>12346</td>
              <td>12/25/17</td>
              <td>John Doe</td>
+             <td>E-Counseling</td>
              <td>P 1500</td>
         
         </tr>
         
     </tbody>
     </table>
-<div id="editor"></div>
 
-    <nav aria-label="Page navigation example">
-  <ul class="pagination justify-content-end">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>
-        
-        <script>
-        swal({
-  title: "Here's a title!",
-});</script>
-    
   
-        
-        
-        
-    </div> <!-- End of container -->
 @endsection
 
 @section ('scripts')
+<script src="{{ asset('js/datatable.js') }}"></script>
+<script src="{{ asset('js/dataTables.buttons.min.js') }}"></script>
+<!-- <script src="{{ asset('js/buttons.flash.min.js') }}"></script> -->
+<script src="{{ asset('js/jszip.min.js') }}"></script>
+<script src="{{ asset('js/pdfmake.min.js') }}"></script>
+<script src="{{ asset('js/vfs_fonts.js') }}"></script>
+<script src="{{ asset('js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('js/buttons.print.min.js') }}"></script>
 <script>
-function printData()
-{
-  var dataToPrint = document.getElementById("printsumTable");
-  newWin = window.open("");
-  newWin.document.write(dataToPrint.outerHTML);
-  newWin.print();
-  newWin.close();
-}
-    
-$('.btnPrint').on('click',function(){
-	printData();
-}) 
+ 
+$('#printsumTable').DataTable({
+  dom: 'Blfrtip',
+  buttons: [
+     'pdf', 'print'
+  ]
+});
+
+
+
+
 </script>
 @endsection
